131fce321ebe5b8c28c5d9e6674243cbdfbc1071
misheMatcha <mishe.vong@gmail.com>
2020-10-31 17:48:22 -0700

    Add logic to display which answer is correct

--- client/src/components/main/question.js
+++ client/src/components/main/question.js
@@ -2,39 +2,98 @@ import React, { useEffect, useState } from 'react';
 import { generateAnswerArray } from '../../util/general_util';
 
 const Question = props => {
-  const answers = generateAnswerArray(props.incorrect, props.correct);
   const [pick, setPick] = useState(null);
   const [hasSubmitted, setHasSubmitted] = useState(false);
+  const [answers, setAnswers] = useState([]);
+  const [nextQuestion, setNextQuestion] = useState(false);
 
   
   useEffect(() => {
+    if(!hasSubmitted && !answers.length) setAnswers(generateAnswerArray(props.incorrect, props.correct));
+    return () => {
+      if(nextQuestion){
+        setAnswers([]);
+        setPick(null);
+        setHasSubmitted(false);
+      }
+    };
   },[]);
 
-  const showChoices = () => {
+  const updatePick = answer => {
+    setPick(answer);
+  };
+
+  const submitAnswer = () => {
+    setHasSubmitted(true);
+  };
+
+  const showChoices = (answers, correct) => {
     return <div>
-      default no selection
+      <ul>
+        {
+          answers.map(answer => {
+            if(pick === null || pick !== answer){
+              return <li key={answer} className='pick-default' onClick={() => updatePick(answer)}>
+                {answer}
+              </li>
+            }else if(pick === answer){
+              return <li key={answer} className='pick-chosen' onClick={() => updatePick(answer)}>
+                {answer}
+              </li>
+            }
+          })
+        }
+      </ul>
+      <button onClick={() => submitAnswer()}>submit</button>
     </div>
   };
 
-  const showResult = () => {
+  const showResult = (answers, correct) => {
     return <div>
-      after submitting
+      <ul>
+        {
+          answers.map(answer => {
+            // if the answer matches the picked answer
+            if(pick === answer){
+              // Then we need to check whether or not it's the correct one
+              // If it is then display it as correct
+              if(pick === correct){
+                return <li key={answer} className='pick-correct'>
+                {answer}
+              </li>
+              }else{
+                // Otherwise display it as incorrect
+                // Let's the user know which answer they got right or wrong
+                return <li key={answer} className='pick-incorrect'>
+                {answer}
+              </li>
+              }
+            }else{
+              // Otherwise check if the current answer is the correct one
+              // If it is show which answer was the correct choice
+              if(answer === correct){
+                return <li key={answer} className='pick-correct'>
+                {answer}
+              </li>
+              }else{
+                // Otherwise use the default class as nothing changed
+                return <li key={answer} className='pick-default'>
+                {answer}
+              </li>
+              }
+            }
+          })
+        }
+      </ul>
+      <button>next question</button>
     </div>
-  };
-
-  const showAnswers = () => {
-    return answers.map(answer => {
-      return <li key={answer}>
-        {answer}
-      </li>
-    });
   };
 
   return <div>
     <h1>Question</h1>
     <p>{props.question}</p>
     {
-      hasSubmitted ? showAnswers() : showChoices()
+      hasSubmitted ? showResult(answers, props.correct) : showChoices(answers, props.correct)
     }
   </div>
 };
